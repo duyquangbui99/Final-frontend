a04bbdbf8d2dd9594c1c4e79198747e296c6dfc0
Quang Bui <77463586+duyquangbui99@users.noreply.github.com>
2024-12-10 13:23:10 -0600

    Update CounterComponent.js

--- src/components/CounterComponent.js
+++ src/components/CounterComponent.js
@@ -1,19 +1,19 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "../redux/counterSlice";
+
 
 function CounterComponent() {
-    const [count, setCount] = React.useState(0);
-
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
-    const reset = () => setCount(0);
+    const count = useSelector((state) => state.counter.value); // Access counter value from Redux store
+    const dispatch = useDispatch(); // Dispatch actions to the store
 
     return (
         <div>
             <h2>Count: {count}</h2>
             <div className="counter-buttons">
-                <button onClick={increment}>Increment</button>
-                <button onClick={decrement}>Decrement</button>
-                <button onClick={reset}>Reset</button>
+                <button onClick={() => dispatch(increment())}>Increment</button>
+                <button onClick={() => dispatch(decrement())}>Decrement</button>
+                <button onClick={() => dispatch(reset())}>Reset</button>
             </div>
         </div>
     );
